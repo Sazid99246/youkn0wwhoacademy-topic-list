57e74be495e88622cc9357e5dcbf1364010373fb
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-06-30 17:32:23 +0600

    module 3 completed

---
+++ 3. Data Types, Variables and Input Outputs/Tutorials/tutorial2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << "Tim" << endl << endl;
+    cout << "Hello World" << "Joe";
+    return 0;
+}
---
+++ 3. Data Types, Variables and Input Outputs/Tutorials/tutorial3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    x = 7;
+    x = 6;
+    x = 8;
+    y = 4;
+
+    cout << x << y;
+    return 0;
+}
+
+/*
+    int 7, 98, -9, -9765, 0, 10
+    float 7.0, -9.8, 0.2, 2.1
+    bool true, false
+    string ""
+    char 'x' '0' 's'
+*/
