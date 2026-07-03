f62a0c7c04b3a5efd2cb2bc7c871ca1e6e8eb3e1
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-07-03 18:29:56 +0600

    matrix problem solved

---
+++ 8. Arrays/Problems/matrix.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int sum1 = 0;
+    int sum2 = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                sum1 += arr[i][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i + j == n - 1)
+            {
+                sum2 += arr[i][j];
+            }
+        }
+    }
+
+    int abs_diff = abs(sum1 - sum2);
+    cout << abs_diff << endl;
+
+    return 0;
+}
