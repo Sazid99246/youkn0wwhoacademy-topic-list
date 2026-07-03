665de684eafa1c74491660562b10ee95dbc701be
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-07-04 04:48:40 +0600

    FIrst Digit problem solved

---
+++ 9. String/Problems/first_digit.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    while (x >= 10)
+    {
+        x /= 10;
+    }
+
+    if (x % 2 == 0)
+    {
+        cout << "EVEN" << endl;
+    }
+    else
+    {
+        cout << "ODD" << endl;
+    }
+
+    return 0;
+}
