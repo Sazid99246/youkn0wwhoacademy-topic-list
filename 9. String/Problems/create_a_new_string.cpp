75333bd57dd230ab7653ee7c728ac067cc09fe06
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-07-03 22:48:51 +0600

    Create a new string problem solved

---
+++ 9. String/Problems/create_a_new_string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+
+    cout << s.length() << " " << t.length() << endl
+         << s << " " << t << endl;
+
+    return 0;
+}
