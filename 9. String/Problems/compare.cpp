0e970d16fae2c233d0acb99dcec8a5e82b2e83e6
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-07-04 07:02:51 +0600

    Compare problem solved

---
+++ 9. String/Problems/compare.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+
+    if (s < t)
+    {
+        cout << s << endl;
+    }
+    else
+    {
+        cout << t << endl;
+    }
+
+    return 0;
+}
