51293706ce9a95f1d420c82715f387196d576bbf
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-07-04 19:36:24 +0600

    Strings problem solved

---
+++ 9. String/Problems/strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+
+    cout << a.size() << " " << b.size() << endl
+         << a + b << endl;
+
+    swap(a[0], b[0]);
+
+    cout << a << " " << b << endl;
+}
