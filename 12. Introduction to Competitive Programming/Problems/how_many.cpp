6b83e74691ad29dd84aca3a7b6caf4acb3c86595
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-07-06 05:42:05 +0600

    How many problem solved

---
+++ 12. Introduction to Competitive Programming/Problems/how_many.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int s, t, count = 0;
+    cin >> s >> t;
+
+    for (int a = 0; a <= s; a++)
+    {
+        for (int b = 0; b <= s; b++)
+        {
+            for (int c = 0; c <= s; c++)
+            {
+                if (a + b + c <= s && a * b * c <= t)
+                {
+                    count++;
+                }
+            }
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
