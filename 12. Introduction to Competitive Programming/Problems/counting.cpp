13a35b97a1a4f33e3099db9e5024016fb25eec13
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-07-05 20:44:58 +0600

    Counting problem solved

---
+++ 12. Introduction to Competitive Programming/Problems/counting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, count = 0;
+    cin >> a >> b;
+
+    for (int i = a; i <= b; i++)
+    {
+        count++;
+    }
+
+    cout << count;
+
+    return 0;
+}
