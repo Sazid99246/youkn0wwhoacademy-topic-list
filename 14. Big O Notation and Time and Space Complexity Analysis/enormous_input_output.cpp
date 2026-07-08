978e501e150bc176d25c74cf10104b08594177ef
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-07-08 10:08:29 +0600

    2 problems solved

---
+++ 14. Big O Notation and Time and Space Complexity Analysis/enormous_input_output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+using namespace ios;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    while (N--) {
+        int a, b;
+        cin >> a >> b;
+        cout << 1LL * a * b << '\n';
+    }
+
+    return 0;
+}
